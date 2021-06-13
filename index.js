0a5c96f3698b94d41ea5ecefd9eaa814039d4609
chesminsky <chesminsky@gmail.com>
2021-06-14 00:08:50 +0300

    fract zoom , size

--- index.js
+++ index.js
@@ -6,31 +6,34 @@ const markers = [
 		lat: 1.22825,
 		lon: 32.171581,
 		title: 'red',
-		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis beatae, minus, hic mollitia accusantium veritatis voluptate perferendis quis temporibus fugiat obcaecati incidunt enim accusamus, pariatur doloribus quibusdam architecto? Reiciendis, nostrum.',
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis beatae, minus, hic mollitia accusantium veritatis voluptate perferendis quis temporibus fugiat obcaecati incidunt enim accusamus, pariatur doloribus quibusdam architecto? Reiciendis, nostrum.',
 		funding: '1000 Euro',
 		status: 'status'
 	},
-    {
+	{
 		id: 2,
 		color: 'blue',
 		size: 5,
 		lat: 1.54825,
 		lon: 33.71581,
 		title: 'blue',
-		description: 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis beatae, minus, hic mollitia accusantium veritatis voluptate perferendis quis temporibus fugiat obcaecati incidunt enim accusamus, pariatur doloribus quibusdam architecto? Reiciendis, nostrum.',
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Reiciendis beatae, minus, hic mollitia accusantium veritatis voluptate perferendis quis temporibus fugiat obcaecati incidunt enim accusamus, pariatur doloribus quibusdam architecto? Reiciendis, nostrum.',
 		funding: '1000 Euro',
 		status: 'status'
 	}
 ];
 
 window.onload = () => {
-	const map = L.map('mapid');
-	map.fitBounds([
-		[4.249611, 29.512469],
-		[-1.456955, 34.708942]
-	]);
+	const map = L.map('mapid', {
+		zoomSnap: 0.5,
+	})
+	.setView([1.4, 32.171581], 7.5);
 
-	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {}).addTo(map);
+
+	L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png').addTo(map);
+
 
 	markers.forEach((m) => {
 		function customTip() {
@@ -45,7 +48,7 @@ window.onload = () => {
 		}
 
 		const cm = L.circleMarker([m.lat, m.lon], {
-			radius: 8,
+			radius: m.size * 2,
 			fillOpacity: 1,
 			color: m.color,
 			fillColor: m.color
