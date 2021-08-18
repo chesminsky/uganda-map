d93ce9dc20bf1566f8621f942221699a9ace557a
chesminsky <chesminsky@gmail.com>
2021-08-18 11:41:20 +0500

    marker fix

--- js/index.js
+++ js/index.js
@@ -1,5 +1,16 @@
 window.onload = init;
 
+const makeMarker = (icon, title, x, y) => {
+	return `
+		<div class="apametsa-map-marker" style="top: ${y}px; left: ${x}px">
+			<img src="img/icons/${icon}.svg" class="apametsa-map-marker-icon-${icon}" />
+			<div class="apametsa-map-marker-title">
+				${title}
+			</div>
+		</div>
+	`
+}
+
 async function init() {
 	const url = './data/markers.json';
 	const response = await fetch(url);
@@ -8,7 +19,7 @@ async function init() {
 	const map = document.querySelector('.apametsa-map');
 
 	data.forEach((m) => {
-		
+		map.innerHTML += makeMarker(m.icon, m.name, m.x, m.y);
 	});
 
 }
