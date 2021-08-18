05fd8ba037bd70350342db9d0e999c7ae77f55c8
chesminsky <chesminsky@gmail.com>
2021-08-18 20:28:29 +0500

    shops

--- js/index.js
+++ js/index.js
@@ -5,29 +5,29 @@ const makeMarker = (icon, title, x, y) => {
 		<div class="apametsa-map-marker" style="top: ${y}px; left: ${x}px">
 			<img src="img/icons/${icon}.svg" class="apametsa-map-marker-icon-${icon}" />
 			<div class="apametsa-map-marker-title">
-				${title}
+				<span>${title}</span>
 			</div>
 		</div>
 	`;
 };
 
-async function init() {
-	const url = './data/markers.json';
-	const response = await fetch(url);
-	const data = await response.json();
+async function load(fileName) {
+	const response = await fetch(`./data/${fileName}.json`);
+	const respData = await response.json();
+	console.log(respData)
+	return respData;
+}
 
+async function loadAll() {
+	return await Promise.all(['shops'].map(fileName => load(fileName)));
+}
+
+async function init() {
+	const data = await loadAll();
+	console.log(data);
 	const map = document.querySelector('.apametsa-map');
 
-	data.forEach((m) => {
+	data.flat().forEach((m) => {
 		map.innerHTML += makeMarker(m.icon, m.name, m.x, m.y);
 	});
-
-	// if (navigator.userAgentData.mobile) {
-	// 	Array.from(document.querySelectorAll('.apametsa-map-marker')).forEach((el) =>
-	// 		el.addEventListener('click', (event) => {
-	// 			event.stopPropagation();
-	// 			el.classList.toggle('is-active');
-	// 		})
-	// 	);
-	// }
 }
